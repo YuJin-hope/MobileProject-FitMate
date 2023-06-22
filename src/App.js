8f4d212e8e73fcc02216953cb6aaa755157417b7
yujin <youjin010402@naver.com>
2023-06-23 00:52:18 +0900

    finalVersion

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from "expo-status-bar";
+import Button, { ButtonTypes } from "./components/Button";
+import { useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { TouchableOpacity } from "react-native";
+import Main from './screens/Main';
+import Login from './screens/Login';
+import Calendar from './screens/Calendar';
+import MyPage from './screens/MyPage';
+import RecordExercise from './screens/RecordExercise';
+import ExerciseVideo from './screens/ExerciseVideo';
+import LearnExercise from './screens/LearnExercise';
+import RecordDiet from './screens/RecordDiet';
+import MenuBar from './screens/MenuBar';
+import RecommendFood from './screens/RecommendFood';
+import CustomModal from './components/CustomModal';
+import RecommendExercise from './screens/RecommendExercise';
+import AI from './screens/AI';
+const Stack = createStackNavigator();
+const App = () => {
+  const [result, setResult] = useState(0);
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Main">
+    <Stack.Screen name="FIT MATE" component={Main} />
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Calendar" component={Calendar} />
+    <Stack.Screen name="MyPage" component={MyPage} />
+    <Stack.Screen name="RecordExercise" component={RecordExercise} />
+    <Stack.Screen name="ExerciseVideo" component={ExerciseVideo} />
+    <Stack.Screen name="LearnExercise" component={LearnExercise} />
+    <Stack.Screen name="RecordDiet" component={RecordDiet} />
+    <Stack.Screen name="MenuBar" component={MenuBar} />
+    <Stack.Screen name="RecommendFood" component={RecommendFood} />
+    <Stack.Screen name="CustomModal" component={CustomModal} />
+    <Stack.Screen name="RecommendExercise" component={RecommendExercise} />
+    <Stack.Screen name="AI" component={AI} />
+    </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
